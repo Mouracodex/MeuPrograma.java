388c2d65cf95c32aad8a0861e6f0a287db7af8be
Pedro Moura <pedrohenriquemoura011@gmail.com>
2024-11-18 21:37:36 -0300

    Add files via upload
    Esse foi meu primeiro programa feito em Java.

---
+++ MeuPrimeiroProgramaJava.java
@@ -0,0 +1,27 @@
+//salvar como MeuPrimeiroProgramaJava.java
+
+//nome da classe
+class MeuPrimeiroProgramaJava
+{
+
+//módulo principal com a entrada pela linha de comando
+public static void main (String entrada [])
+{
+//declaração de variáveis
+      int inteiro = 19
+      char caracter = 'M';
+      double real = 1.75;
+      string frase = "Pedro Henrique ";
+      boolean VF=true;
+
+      if (VF == true)
+{
+      System.out.println ( Eu sou o " + frase + "tenho 	" + inteiro + " anos e tenho " + real + "m de altura");
+}
+
+       System.exit(0);
+
+}
+}  
+
+  
